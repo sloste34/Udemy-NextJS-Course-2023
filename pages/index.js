7f4892f653be495fcddac4c4992f6ffe92013a0b
sloste34 <cocacolazero024@gmail.com>
2023-02-23 23:39:26 +0900

    class 63.Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio'>Go to Portfolio Page</Link>
+        </li>
+        <li>
+          <Link href='/clients'>Go to Clients Page</Link>
+        </li>
+      </ul>
     </div>
   );
 }
