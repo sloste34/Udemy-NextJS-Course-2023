12f2a0693c2dd1844349a6f0b7b3daa212721cb5
sloste34 <cocacolazero024@gmail.com>
2023-02-24 16:10:58 +0900

    class 65.A Different Way Of Setting Link Hrefs

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -13,7 +13,17 @@ function ClientsPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            {/* way no.1 */}
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+            {/* way no.2 */}
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
