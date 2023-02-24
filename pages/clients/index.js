cfb7da7b999c98264d9c289f06e50c78f8744b09
sloste34 <cocacolazero024@gmail.com>
2023-02-24 15:53:12 +0900

    class 64.Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+
 function ClientsPage() {
+  const clients = [
+    { id: 'minsu', name: 'Kim Minsu' },
+    { id: 'manu', name: 'Manuel' },
+    { id: 'chulsu', name: 'KIM Chulsu' },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
