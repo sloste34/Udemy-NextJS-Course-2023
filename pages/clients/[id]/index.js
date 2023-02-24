b3da71773ee3a4fba21cecd74fb02c3117ff9848
sloste34 <cocacolazero024@gmail.com>
2023-02-24 16:33:53 +0900

    class 66.Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,20 @@ function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
 
+  function loadProjectHandler() {
+    router.replace({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {
+        id: 'max',
+        clientprojectid: 'project-a',
+      },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Go To Project</button>
     </div>
   );
 }
